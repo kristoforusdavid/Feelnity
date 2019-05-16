8f7852c77e7cb1f08cc51be7d204e73248e883f8
David <kristoforus_david@outlook.com>
2019-05-16 20:13:43 +0700

    Review

--- CircularLoaderLBTA/ViewController.swift
+++ CircularLoaderLBTA/ViewController.swift
@@ -2,20 +2,26 @@
 //  ViewController.swift
 //  CircularLoaderLBTA
 //
-//  Created by Brian Voong on 12/8/17.
-//  Copyright © 2017 Lets Build That App. All rights reserved.
+//  Created by David on 15/8/19.
+//  Copyright © David. All rights reserved.
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController, URLSessionDownloadDelegate {
-    
+
+
+class ViewController: UIViewController{
+    var audioPlayer = AVAudioPlayer()
     var shapeLayer: CAShapeLayer!
     var pulsatingLayer: CAShapeLayer!
+    var isPlay: Bool = false
+
+    //let icon:UIImage =
     
     let percentageLabel: UILabel = {
         let label = UILabel()
-        label.text = "Start"
+        label.text = "Enjoy"
         label.textAlignment = .center
         label.font = UIFont.boldSystemFont(ofSize: 32)
         label.textColor = .white
@@ -39,19 +45,52 @@ class ViewController: UIViewController, URLSessionDownloadDelegate {
         let circularPath = UIBezierPath(arcCenter: .zero, radius: 100, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
         layer.path = circularPath.cgPath
         layer.strokeColor = strokeColor.cgColor
-        layer.lineWidth = 20
+        layer.lineWidth = 60
         layer.fillColor = fillColor.cgColor
         layer.lineCap = kCALineCapRound
         layer.position = view.center
+        layer.borderColor = fillColor.cgColor
+        layer.backgroundColor = fillColor.cgColor
+        layer.borderWidth = 50
+        layer.shadowOpacity = 5.0
+        layer.shadowColor = #colorLiteral(red: 0.5065737963, green: 0.8325939775, blue: 0.2896941006, alpha: 0.7629227312)
+        layer.lineCap = kCAGravityCenter
+        
         return layer
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //import audio library
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "Serenity", ofType: "mp3")!))
+            
+            let audioSession = AVAudioSession.sharedInstance()
+            do{
+               try audioSession.setCategory(AVAudioSessionCategoryPlayback)
+                print(audioPlayer.duration)
+            }
+            catch{
+       
+            }
+        }
+        catch {
+            print(error)
+        }
+    
+        //setupNotificationObservers()
         
-        setupNotificationObservers()
+        //self.view.backgroundColor = #colorLiteral(red: 0, green: 0.9768045545, blue: 0.8095836901, alpha: 0.7993899829)
         
-        view.backgroundColor = UIColor.backgroundColor
+        //Bakground Options Solid or Gradient :
+        
+        //view.backgroundColor = UIcolor.backgroundColor
+        let layer1 = CAGradientLayer()
+        layer1.frame = view.bounds
+        layer1.colors = [UIColor.yellow.cgColor, UIColor.green.cgColor, UIColor.yellow.cgColor]
+        layer1.startPoint = CGPoint(x: 0, y: 0)
+        layer1.endPoint = CGPoint(x:1, y:1)
+        view.layer.addSublayer(layer1)
         
         setupCircleLayers()
         
@@ -60,27 +99,29 @@ class ViewController: UIViewController, URLSessionDownloadDelegate {
         setupPercentageLabel()
     }
     
+    
     private func setupPercentageLabel() {
         view.addSubview(percentageLabel)
         percentageLabel.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
         percentageLabel.center = view.center
+        percentageLabel.shadowColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
     }
     
     private func setupCircleLayers() {
-        pulsatingLayer = createCircleShapeLayer(strokeColor: .clear, fillColor: UIColor.pulsatingFillColor)
+        pulsatingLayer = createCircleShapeLayer(strokeColor: #colorLiteral(red: 0.5, green: 1, blue: 0.5235980308, alpha: 0.6971318493), fillColor: #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 0.8002461473))
         view.layer.addSublayer(pulsatingLayer)
         animatePulsatingLayer()
         
-        let trackLayer = createCircleShapeLayer(strokeColor: .trackStrokeColor, fillColor: .backgroundColor)
+        let trackLayer = createCircleShapeLayer(strokeColor: #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1), fillColor: #colorLiteral(red: 0.9994240403, green: 0.9855536819, blue: 0, alpha: 0.7279002568))
         view.layer.addSublayer(trackLayer)
         
-        shapeLayer = createCircleShapeLayer(strokeColor: .outlineStrokeColor, fillColor: .clear)
-        
+        shapeLayer = createCircleShapeLayer(strokeColor: #colorLiteral(red: 0.9994240403, green: 0.9855536819, blue: 0, alpha: 0.2497592038), fillColor: #colorLiteral(red: 0.6271087527, green: 0.8490449786, blue: 0.4607692957, alpha: 1))
+    
         shapeLayer.transform = CATransform3DMakeRotation(-CGFloat.pi / 2, 0, 0, 1)
-        shapeLayer.strokeEnd = 0
+        shapeLayer.strokeEnd = 10
         view.layer.addSublayer(shapeLayer)
     }
-    
+    // init circle animation
     private func animatePulsatingLayer() {
         let animation = CABasicAnimation(keyPath: "transform.scale")
         
@@ -89,47 +130,66 @@ class ViewController: UIViewController, URLSessionDownloadDelegate {
         animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
         animation.autoreverses = true
         animation.repeatCount = Float.infinity
+        animation.speed = 1
         
-        pulsatingLayer.add(animation, forKey: "pulsing")
+        
+        pulsatingLayer.add(animation, forKey: "Pulsing")
     }
     
-    let urlString = "https://firebasestorage.googleapis.com/v0/b/firestorechat-e64ac.appspot.com/o/intermediate_training_rec.mp4?alt=media&token=e20261d0-7219-49d2-b32d-367e1606500c"
+//    let urlString = "https://firebasestorage.googleapis.com/v0/b/firestorechat-e64ac.appspot.com/o/intermediate_training_rec.mp4?alt=media&token=e20261d0-7219-49d2-b32d-367e1606500c"
     
-    private func beginDownloadingFile() {
-        print("Attempting to download file")
+    // Progress Bar
+    private func beginPlayMusic() {
+        print("Attempting to play file")
         
-        shapeLayer.strokeEnd = 0
+        shapeLayer.strokeEnd = 00
         
-        let configuration = URLSessionConfiguration.default
-        let operationQueue = OperationQueue()
-        let urlSession = URLSession(configuration: configuration, delegate: self, delegateQueue: operationQueue)
+        //let configuration = URLSessionConfiguration.default
+        //let operationQueue = OperationQueue()
+//        let urlSession = URLSession(configuration: configuration, delegate: self, delegateQueue: operationQueue)
         
-        guard let url = URL(string: urlString) else { return }
-        let downloadTask = urlSession.downloadTask(with: url)
-        downloadTask.resume()
-    }
-    
-    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
-        let percentage = CGFloat(totalBytesWritten) / CGFloat(totalBytesExpectedToWrite)
+//        guard let url = URL(string: urlString) else { return }
+//        let downloadTask = urlSession.downloadTask(with: url)
+//        downloadTask.resume()
+       // let percentage = Float.random(in: 1...100)/100
+       
+        
+        //Init Music Player
+        
+        audioPlayer.play()
+        audioPlayer.currentTime = 0
         
         DispatchQueue.main.async {
-            self.percentageLabel.text = "\(Int(percentage * 100))%"
-            self.shapeLayer.strokeEnd = percentage
+            //self.percentageLabel.text = "\(Int(percentage * 100))%"
+            
+            // self.shapeLayer.strokeEnd = CGFloat(percentage)
+            self.shapeLayer.strokeEnd = CGFloat(00)
+            
         }
-        
-        print(percentage)
     }
     
-    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
-        print("Finished downloading file")
-    }
+//    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
+//        let percentage = CGFloat(totalBytesWritten) / CGFloat(totalBytesExpectedToWrite)
+//
+//        DispatchQueue.main.async {
+//            self.percentageLabel.text = "\(Int(percentage * 100))%"
+//            self.shapeLayer.strokeEnd = percentage
+//        }
+    
+//        print(percentage)
+//    }
+    
+    //func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+        //print("Finished playing file")
+   // }
     
     fileprivate func animateCircle() {
         let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
         
+        let durasi = audioPlayer.duration
         basicAnimation.toValue = 1
         
-        basicAnimation.duration = 2
+        basicAnimation.duration = durasi
         
         basicAnimation.fillMode = kCAFillModeForwards
         basicAnimation.isRemovedOnCompletion = false
@@ -139,10 +199,18 @@ class ViewController: UIViewController, URLSessionDownloadDelegate {
     
     @objc private func handleTap() {
         print("Attempting to animate stroke")
+    
+        if isPlay == false{
+            beginPlayMusic()
+            animateCircle()
+            isPlay = true
+        }
+        else
+        {
+            audioPlayer.stop()
+            isPlay = false
+        }
         
-        beginDownloadingFile()
-        
-//        animateCircle()
     }
 
 }
