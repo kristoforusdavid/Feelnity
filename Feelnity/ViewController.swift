7b7654b752c647c0ddda3d462bd15c2318733a5d
David <kristoforus_david@outlook.com>
2019-05-31 14:15:30 +0700

    Initial Commit

---
+++ Feelnity/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Feelnity
+//
+//  Created by Kristoforus David on 31/05/19.
+//  Copyright © 2019 Kristoforus David. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
